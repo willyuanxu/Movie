8a04b1ad0e7acaaf71cf0539826f181ac8467323
Qing Ye <yeqing@usc.edu>
2018-11-12 13:30:02 -0800

    Create Yuanx_yeqing_MainApp.java

---
+++ src/application/Yuanx_yeqing_MainApp.java
@@ -0,0 +1,17 @@
+package application;
+
+public class Yuanx_yeqing_MainApp {
+	
+	
+	//set up components, add accessibility text
+	private void setUpCenterPane() {
+		
+	}
+	
+	private void setUpListView() {
+		
+	}
+	
+	
+
+}
